23184c28dcdfa16d2476ab0dbe88246e8c3a4814
hungnvph50552 <hungnvph50552@gmail.com>
2024-11-13 21:15:12 +0700

    Add files via upload

---
+++ chitietdonhang/chi_tiet_don_hangs.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 13, 2024 at 01:36 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `base_du_an_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chi_tiet_don_hangs`
+--
+
+CREATE TABLE `chi_tiet_don_hangs` (
+  `id` int NOT NULL,
+  `don_hang_id` int NOT NULL,
+  `san_pham_id` int NOT NULL,
+  `don_gia` decimal(10,2) NOT NULL,
+  `so_luong` int NOT NULL,
+  `thanh_tien` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `chi_tiet_don_hangs`
+--
+
+INSERT INTO `chi_tiet_don_hangs` (`id`, `don_hang_id`, `san_pham_id`, `don_gia`, `so_luong`, `thanh_tien`) VALUES
+(1, 1, 1, '500.00', 1, '500.00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chi_tiet_don_hangs`
+--
+ALTER TABLE `chi_tiet_don_hangs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chi_tiet_don_hangs`
+--
+ALTER TABLE `chi_tiet_don_hangs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ quanlidonhang/don_hangs.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 13, 2024 at 01:36 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `base_du_an_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don_hangs`
+--
+
+CREATE TABLE `don_hangs` (
+  `id` int NOT NULL,
+  `ma_don_hang` varchar(50) NOT NULL,
+  `tai_khoan_id` int NOT NULL,
+  `ten_nguoi_nhan` varchar(255) NOT NULL,
+  `email_nguoi_nhan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `sdt_nguoi_nhan` varchar(15) NOT NULL,
+  `dia_chi_nguoi_nhan` text NOT NULL,
+  `ngay_dat` date NOT NULL,
+  `tong_tien` decimal(10,2) NOT NULL,
+  `ghi_chu` text,
+  `phuong_thuc_thanh_toan_id` int NOT NULL,
+  `trang_thai_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `don_hangs`
+--
+
+INSERT INTO `don_hangs` (`id`, `ma_don_hang`, `tai_khoan_id`, `ten_nguoi_nhan`, `email_nguoi_nhan`, `sdt_nguoi_nhan`, `dia_chi_nguoi_nhan`, `ngay_dat`, `tong_tien`, `ghi_chu`, `phuong_thuc_thanh_toan_id`, `trang_thai_id`) VALUES
+(1, 'DH_001', 1, 'Phạm Văn Tam', 'TamML123@gmail.com', '0381111111', 'Hinode Royal Park', '2024-11-11', '500.00', 'giao nhanh dùm mình', 2, 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `don_hangs`
+--
+ALTER TABLE `don_hangs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `don_hangs`
+--
+ALTER TABLE `don_hangs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
